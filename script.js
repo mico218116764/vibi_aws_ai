c426d0d9c37de107590db830a6d872a39089f765
Ricky Alverdo <102291249+ClostridiumR@users.noreply.github.com>
2022-12-06 11:11:21 +0700

    Create image preview
    Create image preview from input file

--- script.js
+++ script.js
@@ -13,5 +13,17 @@ function fileValidation(file) {
         fileInput.value = '';
         return false;
     }
-    else true;
+    else {
+        // Image preview
+        if (fileInput.files && fileInput.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                document.getElementById(
+                    'imagePreview').innerHTML =
+                    '<img src="' + e.target.result
+                    + '" height="200px"/>';
+            };
+            reader.readAsDataURL(fileInput.files[0]);
+        }
+    }
 }
